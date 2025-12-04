7a3683124c9cb4bc6fba9304573d9e048f1f3f32
Fillz <filip.asberg@gmail.com>
2025-12-04 10:06:47 +0100

    Solve day 4

---
+++ day04/part2.py
@@ -0,0 +1,39 @@
+map = [list(line) for line in open(0).read().splitlines()]
+map_width = len(map[0])
+map_height = len(map)
+
+def is_paper(y, x):
+  if x < 0 or y < 0 or x > map_width - 1 or y > map_height - 1:
+    return False
+  if map[y][x] == '@':
+    return True
+  return False
+
+def remove_paper(y, x):
+  map[y][x] = '.'
+
+def find_and_remove_paper():
+  accessible = 0
+  for y in range(0, map_height):
+    for x in range(0, map_width):
+      if not is_paper(y, x):
+        continue
+      adjacents = 0
+      for i in range(y - 1, y + 2):
+        for j in range(x - 1, x + 2):
+          if i == y and j == x:
+            continue
+          if is_paper(i, j):
+            adjacents += 1
+      if adjacents < 4:
+        accessible += 1
+        remove_paper(y, x)
+  return accessible
+
+total = 0
+while True:
+  removed = find_and_remove_paper()
+  if removed == 0:
+    break
+  total += removed
+print(total)
